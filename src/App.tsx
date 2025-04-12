f934f90345f5761c123ed9971fd371fac7c48fde
Mariano Martinotti <mariano.andres1024@gmail.com>
2025-04-12 12:05:06 -0300

    insert for add github pages

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
 
   return (
-    <Router basename='/apoloixcode/'>
+    <Router>
           <Navbar />
           <Routes>
                 <Route path='/' element={<Home />} />
